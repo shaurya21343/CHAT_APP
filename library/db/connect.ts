bb9dfe66d0ed01e65ae305d51978b05dab5f3ea8
Shaurya <smms1917@gmail.com>
2025-08-10 15:23:58 +0530

    Update connect.ts

--- library/db/connect.ts
+++ library/db/connect.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = "mongodb+srv://smms1917:smms@cluster0.exhejsz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const MONGODB_URI = process.env.MONGO_URI;
 
 if (!MONGODB_URI) {
     throw new Error('Please define the MONGODB_URI environment variable');
